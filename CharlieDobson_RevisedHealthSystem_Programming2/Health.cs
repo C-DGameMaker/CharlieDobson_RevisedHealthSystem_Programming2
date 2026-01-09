3c9ccfdf50473c31ed00fe2795ccf0a07cbc3264
C-DGameMaker <charlotte5634@outlook.com>
2026-01-09 11:54:55 -0400

    Health Class Made

---
+++ CharlieDobson_RevisedHealthSystem_Programming2/Health.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace CharlieDobson_RevisedHealthSystem_Programming2
+{
+    internal class Health
+    {
+        //Properties
+        int CurrentHealth { get; set; }
+        int MaxHealth { get; set; }
+
+        //Constructor
+        public Health(int maxHealth)
+        {
+            MaxHealth = maxHealth;
+            CurrentHealth = MaxHealth;
+        }
+
+        //Methods
+        public void TakeDamage(int damageAmount)
+        {
+            if(damageAmount < 0)
+            {
+                Console.Write("WARNING! A player cannot take negative damage.");
+                damageAmount = 0;
+            }
+
+            CurrentHealth -= damageAmount;
+        }
+
+        public void Heal(int healAmount)
+        {
+            if (healAmount < 0)
+            {
+                Console.Write("WARNING! A player cannot heal a negative amount.");
+                healAmount = 0;
+            }
+
+            if(healAmount + CurrentHealth > MaxHealth)
+            {
+                CurrentHealth = MaxHealth;
+            }
+            else
+            {
+                CurrentHealth += healAmount;
+            }
+
+            
+        }
+
+        public void Restore()
+        {
+            CurrentHealth = MaxHealth;
+        }
+
+    }
+}
